cb6cc6a67f721c58ab139ac6991ce949f66fd46e
jonathan <jon.rey8995@gmail.com>
2018-07-04 19:23:20 -0400

    primeros metodos en python (04-07-2018)

---
+++ clase.py
@@ -0,0 +1,31 @@
+print "hola mundo"
+a=3
+b=8
+c=a+b
+print "el resultado es:" + str(c)
+
+
+iva = 10.
+harina = 5
+r=(iva*harina)/100
+rf=harina+r
+print rf
+print "iva" + str(r)
+
+
+a=10
+b=10
+if a>b:
+	print a
+else:
+	print b
+	
+
+
+
+suma=1
+for i in range(1,101):
+	print i
+
+for e in "hola mundo":
+	print e
