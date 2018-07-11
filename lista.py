165eb92ff3b54915e47ad547314c09f138a0f62e
Jeison Pernia <jeisonestudio@gmail.com>
2018-07-11 19:32:38 -0400

    Grupo Machete

---
+++ lista.py
@@ -0,0 +1,16 @@
+print "Porfavor indiquenos la cantidad de de palabras que quieres asignar"
+palabras = input()
+print "Digame cuantas palabras tiene la lista =" + str(palabras)
+lista = []
+for i in range (palabras):
+    palabras2= raw_input ("Digame la palabra " + str(i+1) + " ")
+    lista.append(palabras2)
+
+print "lista =" , lista
+
+
+
+
+
+
+
